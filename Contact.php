d115e78efa690c20d1afc080a99ffa638b7ca3ad
fagner <fanig_01@hotmail.com>
2014-09-10 15:28:00 -0300

    Add methods get and sets

--- Contact.php
+++ Contact.php
@@ -2,7 +2,52 @@
 
 class Contact
 {
-	
+	// construct methods gets and sets
+	function_construct()
+	{
+		parent::Contact();
+	}
+
+	public function	__set($idContact)
+	{
+		$this->$idContact;
+	}
+
+	public function	__set($nameContact)
+	{
+		$this->$nameContact;
+	}
+
+	public function	__set($phoneContact)
+	{
+		$this->$phoneContact;
+	}
+
+	public function	__set($emailContact)
+	{
+		$this->$emailContact;
+	}
+
+
+		public function	get($idContact)
+	{
+		return $this->$idContact;
+	}
+
+	public function	get($nameContact)
+	{
+		return $this->$nameContact;
+	}
+
+	public function	get($phoneContact)
+	{
+		return $this->$phoneContact;
+	}
+
+	public function	get($emailContact)
+	{
+		return $this->$emailContact;
+	}
 }
 
 ?>
