a7cd7a9c485f8a2565a49684bb654b9a6c7f98c8
Fillipe Oliveira Feitosa <fillfeitosa@gmail.com>
2014-09-10 15:19:16 -0300

    Delete Contact From Agenda Function

--- Agenda.php
+++ Agenda.php
@@ -2,7 +2,18 @@
 
 class Agenda
 {
-
+    private $db = array();
+    
+    
+    /* Delete contact from the agenda  */
+    public function deleteContact($paramId){
+        
+        if(!isset($paramId)){
+            unset($this->db[$paramId]);
+        }else{
+            $errormsg = "There is no Contact for this ID!";
+        }
+    }
 }
 
 ?>
