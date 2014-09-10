20ef8a06ea933ff4fe942dfd9bf1b1fa3fd9953f
larissa <larissax55@gmail.com>
2014-09-10 15:17:35 -0300

    Added edit

--- Agenda.php
+++ Agenda.php
@@ -2,7 +2,13 @@
 
 class Agenda
 {
-
+    public function edit(Contact $contact)
+    {
+      $obj = $db[$contact->$id];
+      $obj->name = $contact->name;
+      $obj->cellPhone = $contact->cellPhone;
+      $obj->email = $contact->email;    
+    }
 }
 
 ?>
