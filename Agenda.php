9327f8dbc270d963941b48afd6c770a493f9561b
carol15022 <carol15022@hotmail.com>
2014-09-10 15:27:56 -0300

    Function Add contacts

--- Agenda.php
+++ Agenda.php
@@ -2,6 +2,18 @@
 
 class Agenda
 {
+	/** Add contact from the agenda */
+
+	public function addContact ($paramId, Contact $contact) {
+		if (!isset($paramId) || !isset($contact)){
+			this->$db[&paramId] = $contact;
+		}
+
+		else{
+			echo "Error: Contact not added. Null parameters.";
+		}
+		
+	}
 
 }
 
