5fb2f8c5d6c133b73d943f04e3a9905b95fc7cfd
Charles Oliveira <18oliveira.charles@gmail.com>
2014-09-10 15:26:20 -0300

    Added search

--- Agenda.php
+++ Agenda.php
@@ -3,6 +3,18 @@
 class Agenda
 {
 
+	public function search(Contact $contact)
+	{
+		$size = sizeof($db);
+		for($i = 0; $i < $size; $i++)
+		{
+			$obj = $db[$i];
+			if($obj->name == $contact->name || 
+			   $obj->email == $contact->email ||
+			   $obj->phoneContact == $contact->phoneContact)
+				return $obj;
+		}
+	}
 }
 
 ?>
